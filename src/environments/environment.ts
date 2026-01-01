57594c3abc725286085dfd05affae99d3a7477c2
MrBrownIT <marronecristian@gmail.com>
2026-01-01 20:24:15 +0100

    change api 2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
     //geminiApiKey: 'PAIzaSyDce6pe8wzmHG-gD2pjScVa1mNUgik95Hk'  // ← Sostituisci con vera API key 
-    geminiApiKey: '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'
+    geminiApiKey: 'AIzaSyDce6pe8wzmHG-gD2pjScVa1mNUgik95Hk' //'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'
   };
